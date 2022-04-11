9be5ce01bab6cf3f9d05bcee4084acf46d344791
Hossen <mdmitho199911@gmail.com>
2022-04-11 15:33:43 +0600

    css media query

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import './Service.css'
 
 const Service = ({service}) => {
     const {name,img,description, price} = service
     return (
-        <div>
+        <div className='service'>
             <img src={img} alt="" />
-            <h1>This is service :{name}</h1>
+            <h1>{name}</h1>
+            <p>Price : {price}</p>
+            <p><small>{description}</small></p>
+            <button>Book : {name}</button>
         </div>
     );
 };
